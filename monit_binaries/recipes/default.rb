bb3702cd82599b8bc92c9e41f3ad46cb7bd9196d
sawanoboly <sawanoboriyu@higanworks.com>
2012-08-15 19:51:37 -0700

    bash -e flag

--- monit_binaries/recipes/default.rb
+++ monit_binaries/recipes/default.rb
@@ -16,19 +16,22 @@
 end
 
 
-bash "install_from_source" do
+script "install_from_source" do
+  interpreter "bash"
   user "root"
   Chef::Log.info("Start: install monit-#{node[:monit][:version]}")
+  flags "-e"
   code <<-"EOH"
-      cd /usr/local/src/monit/                                                                                            && \
-      wget http://mmonit.com/monit/dist/binary/#{node[:monit][:version]}/#{node[:monit][:binaries]}.tar.gz    && \
-      tar xvzf #{node["monit"][:binaries]}.tar.gz                                                              && \
-      cp -f monit-#{node[:monit][:version]}/bin/monit /usr/sbin/                                                          && \
-      cp -f monit-#{node[:monit][:version]}/man/man1/monit.1 /usr/share/man/man1                                          && \
+      ehco
+      cd /usr/local/src/monit/
+      wget http://mmonit.com/monit/dist/binary/#{node[:monit][:version]}/#{node[:monit][:binaries]}.tar.gz
+      tar xvzf #{node["monit"][:binaries]}.tar.gz
+      cp -f monit-#{node[:monit][:version]}/bin/monit /usr/sbin/
+      cp -f monit-#{node[:monit][:version]}/man/man1/monit.1 /usr/share/man/man1
       mandb
   EOH
 
-  only_if "test ! -f #{node[:monit][:binaries]}.tar.gz"
+  only_if "test ! -f /usr/local/src/monit/#{node[:monit][:binaries]}.tar.gz"
   Chef::Log.info("End: install monit-#{node[:monit][:version]}")
 end
 

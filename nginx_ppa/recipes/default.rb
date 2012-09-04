c8dd44722c88069bc907e54e03d561114623ed19
sawanoboly <sawanoboriyu@higanworks.com>
2012-09-03 18:30:21 -0700

    nginx-ppa: add apt repository

---
+++ nginx_ppa/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: nginx_ppa
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+#
+include_recipe "apt"
+
+apt_repository "nginx-ppa" do
+  uri "http://ppa.launchpad.net/nginx/stable/ubuntu"
+  distribution node.lsb.codename
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "C300EE8C"
+end 
